9363f36be2143267fa18a371e0dc80485eb3e5d5
Fletcher Chapin <fletchapin@gmail.com>
2022-08-30 14:58:11 -0700

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     extras_require=extra_requirements,
     test_suite="tests",
     url="https://github.com/we3lab/wwtp-configuration",
-    version="0.0.8",
+    version="0.0.9",
     zip_safe=False,
 )
