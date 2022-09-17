5ab9d676c37e3d1b1e3264bcb53f7f1263947b8d
Fletcher Chapin <fletchapin@gmail.com>
2022-09-16 17:27:11 -0700

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     extras_require=extra_requirements,
     test_suite="tests",
     url="https://github.com/we3lab/wwtp-configuration",
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
 )
--- wwtp_configuration/__init__.py
+++ wwtp_configuration/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Fletcher Chapin"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 def get_module_version():
