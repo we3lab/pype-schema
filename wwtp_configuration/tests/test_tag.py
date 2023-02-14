182d9a8efb951fc6cc3d59af42cfd3db6e09c614
Fletcher Chapin <fletchapin@gmail.com>
2023-02-13 19:56:24 -0800

    Added a test for VirtualTag units

--- wwtp_configuration/tests/test_tag.py
+++ wwtp_configuration/tests/test_tag.py
@@ -3,6 +3,7 @@ import pint
 import pytest
 import pandas as pd
 from wwtp_configuration.units import u
+from wwtp_configuration.utils import parse_units
 from wwtp_configuration.tag import VirtualTag
 from wwtp_configuration.parse_json import JSONParser
 
@@ -17,31 +18,33 @@ pint.set_application_registry(u)
 
 @pytest.mark.skipif(skip_all_tests, reason="Exclude all tests")
 @pytest.mark.parametrize(
-    "json_path, csv_path, tag_name, expected_path",
+    "json_path, csv_path, tag_name, expected_path, expected_units",
     [
         (
             "../data/sample.json",
             "data/sample_data.csv",
             "GrossGasProduction",
-            "data/gross_gas.csv"
+            "data/gross_gas.csv",
+            "SCFM"
         ),
         (
             "../data/sample.json",
             "data/sample_data.csv",
             "ElectricityProductionByGasVolume",
-            "data/electrical_efficiency.csv"
+            "data/electrical_efficiency.csv",
+            "kilowatt * hour * minute / (feet ** 3)"
         ),
     ],
 )
-def test_calculate_values(json_path, csv_path, tag_name, expected_path):
+def test_calculate_values(json_path, csv_path, tag_name, expected_path, expected_units):
     parser = JSONParser(json_path)
     result = parser.initialize_network()
     tag = result.get_tag(tag_name, recurse=True)
-    
+
     # TODO: add numpy array and list test cases
     data = pd.read_csv(csv_path)
     expected = pd.read_csv(expected_path)
 
     pd.testing.assert_series_equal(tag.calculate_values(data), expected[tag_name])
 
-    # TODO: check expected units are correct
+    assert parse_units(expected_units) == tag.units
