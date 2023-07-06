1bd705b494b92e97d5f04ca7524a02ecd89bdb63
Fletcher Chapin <fletchapin@gmail.com>
2023-07-05 20:53:42 -0700

    Bump version: 0.2.3 → 0.3.0

--- wwtp_configuration/__init__.py
+++ wwtp_configuration/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Fletcher Chapin"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 def get_module_version():
