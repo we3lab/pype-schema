8dcfbe4ff6b98c0ca3d1f17b92a9e516a725dca4
Fletcher Chapin <fletchapin@gmail.com>
2024-01-11 19:14:53 -0800

    Got a few more tests passing

--- pype_schema/tests/test_parse_json.py
+++ pype_schema/tests/test_parse_json.py
@@ -3,7 +3,7 @@ import pint
 import pytest
 import json
 import pickle
-from pype_schema.units import ug
+from pype_schema.units import u
 from pype_schema.parse_json import JSONParser
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -24,10 +24,10 @@ pint.set_application_registry(u)
         ("data/sample_nested_vtag.json", "data/sample_nested_vtag.pkl"),
     ],
 )
-def test_create_json_path(json_path, expected_path):
+def test_create_network(json_path, expected_path):
     parser = JSONParser(json_path)
     try:
-        result = parser.initialize()
+        result = parser.initialize_network()
         with open(expected_path, "rb") as pickle_file:
             expected = pickle.load(pickle_file)
     except Exception as err:
@@ -63,16 +63,16 @@ def test_create_json_path(json_path, expected_path):
         ),
     ],
 )
-def test_merge(json_path, original_network_path, node_id, inplace, expected_path):
+def test_merge_network(json_path, original_network_path, node_id, inplace, expected_path):
     parser = JSONParser(json_path)
-    expected = JSONParser(expected_path).initialize()
+    expected = JSONParser(expected_path).initialize_network()
     if node_id:
-        original = JSONParser(original_network_path).initialize().get_node(node_id)
+        original = JSONParser(original_network_path).initialize_network().get_node(node_id)
         expected = expected.get_node(node_id)
     else:
         original = original_network_path
 
-    result = parser.merge(original, inplace=inplace)
+    result = parser.merge_network(original, inplace=inplace)
     assert result == expected
     assert inplace == (expected == parser.network_obj)
 
@@ -82,7 +82,7 @@ def test_merge(json_path, original_network_path, node_id, inplace, expected_path
 def test_to_json(
     json_path,
 ):
-    expected = JSONParser(json_path).initialize()
+    expected = JSONParser(json_path).initialize_network()
     JSONParser.to_json(expected, "data/test_to_json.json")
-    result = JSONParser("data/test_to_json.json").initialize()
+    result = JSONParser("data/test_to_json.json").initialize_network()
     assert result == expected
