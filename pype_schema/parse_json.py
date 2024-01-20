517af67e99068337a09717bc9506d373b97a88ea
jbolorinos <josebolorinos@gmail.com>
2024-01-19 17:47:18 -0800

    Print message for debugging, will remove

--- pype_schema/parse_json.py
+++ pype_schema/parse_json.py
@@ -570,6 +570,7 @@ class JSONParser:
             )
 
         tags = self.config[connection_id].get("tags")
+        print("connection_id: ", connection_id)
         print("connection_ob: ", connection_obj)
         print("type(connection_obj): ", type(connection_obj))
         exit_point_id = (
