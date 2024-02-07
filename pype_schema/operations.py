6249045d7cd950c9421481d4bd8c351ab7eb9028
jbolorinos <josebolorinos@gmail.com>
2024-02-07 10:07:36 -0800

    Get change operations function (modified) and new operations test script that tests it

--- pype_schema/operations.py
+++ pype_schema/operations.py
@@ -4,7 +4,7 @@ import scipy as sp  # noqa: F401
 import warnings
 
 
-def get_change(variable, delta_t=1, split=False, pos_change=True, neg_change=True):
+def get_change(variable, delta_t=1, split=False):
     """Converts cumulative value to rate-of-change value using finite differences
     Note: assumes rate of change at time t is equal to the difference between
     the value at time t+1 and t
@@ -17,10 +17,6 @@ def get_change(variable, delta_t=1, split=False, pos_change=True, neg_change=Tru
         Time difference between two consecutive values of the variable (default is 1)
     split: bool
         Whether to split the variable into a negative change and a positive change
-    neg_change: bool
-        Whether to return the negative change variable
-    pos_change: bool
-        Whether to return the positive change variable
 
     Returns
     -------
@@ -28,23 +24,16 @@ def get_change(variable, delta_t=1, split=False, pos_change=True, neg_change=Tru
         Rate of change variable or tuple of netative, positive rate of change variable
     """
     variable = variable.values if isinstance(variable, pd.Series) else variable
+    print("variable", variable)
     change = (variable[1:] - variable[:-1]) / delta_t
     change = np.concatenate([change, np.array([np.nan])])
+    print("change", change)
+    change_neg, change_pos = change.copy(), change.copy()
+    change_neg[change_neg > 0] = 0
+    change_neg = -change_neg
+    change_pos[change_pos < 0] = 0
     if split:
-        change_neg, change_pos = change.copy(), change.copy()
-        change_neg[change_neg > 0] = 0
-        change_neg = -change_neg
-        change_pos[change_pos < 0] = 0
-        if pos_change and neg_change:
-            return change_neg, change_pos
-        elif neg_change:
-            return change_neg
-        elif pos_change:
-            return change_pos
-        else:
-            warnings.warn(
-                """Split is `True` but both pos_change and neg_change are `False`
-                    so no variable is returned"""
-            )
+        return change_neg, change_pos
     else:
         return change
+
