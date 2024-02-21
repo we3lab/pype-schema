354c9d3c374e514da195dd6c51c777ece9783411
Fletcher Chapin <fletchapin@gmail.com>
2024-02-20 18:30:08 -0800

    Fixed the pickling error with old Pint version

--- pype_schema/visualize.py
+++ pype_schema/visualize.py
@@ -133,7 +133,7 @@ def draw_graph(network, pyvis=False):
         g.add_nodes_from(legend_nodes)
 
         nt.from_nx(g)
-        nt.show(network.id + ".html", notebook=False)
+        nt.show(network.id + ".html")
     else:
         # create legend
         custom_lines = []
