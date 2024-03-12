f046c6cdbc22d67613938850441308a174d8fa74
Fletcher Chapin <fletchapin@gmail.com>
2024-03-12 14:42:02 -0700

    Bump version: 0.4.0 → 0.5.0

--- pype_schema/__init__.py
+++ pype_schema/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Fletcher Chapin"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 
 def get_module_version():
