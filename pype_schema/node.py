51d44f48e39fb0cfcd4624a943c46992c35387ae
Fletcher Chapin <fletchapin@gmail.com>
2024-05-07 08:31:30 -0700

    Trying to avoid infinite recursion

--- pype_schema/node.py
+++ pype_schema/node.py
@@ -1314,7 +1314,7 @@ class Pump(Node):
             "Please switch from `horsepower` to new `power_rating` attribute",
             DeprecationWarning,
         )
-        return self.horsepower
+        return getattr(self, horsepower)
 
     def set_horsepower(self, horsepower):
         warnings.warn(
@@ -1627,7 +1627,7 @@ class Battery(Node):
             "Please switch from `capacity` to new `energy_capacity` attribute",
             DeprecationWarning,
         )
-        return self.capacity
+        return getattr(self, capacity)
 
     def set_capacity(self, capacity):
         warnings.warn(
