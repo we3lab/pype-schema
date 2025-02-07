da3fb1e44ce72c92bf63e2ac6e1fe1396cc2f17d
LilyLiu0719 <b06901004@ntu.edu.tw>
2025-02-06 16:30:45 -0800

    update epyt_utils

--- pype_schema/epyt_utils.py
+++ pype_schema/epyt_utils.py
@@ -1,14 +1,13 @@
 from epyt import epanet
-import sys
+
 import json
 import numpy as np
+from collections import defaultdict
 
 content_placeholder = "DrinkingWater"
 
-
 class NpEncoder(json.JSONEncoder):
     """Custom JSON encoder for numpy types"""
-
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
@@ -19,7 +18,7 @@ class NpEncoder(json.JSONEncoder):
         return super(NpEncoder, self).default(obj)
 
 
-def epyt2pypes(inp_file, out_file, add_nodes=False):
+def epyt2pypes(inp_file, out_file, add_nodes=False, use_name_as_id=False):
     """Convert an EPANET input file to a PYPES JSON file
 
     Parameters
@@ -32,11 +31,17 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
 
     add_nodes : bool
         Whether to add additional nodes of Pumps
+
+    use_name_as_id : bool
+        Whether to use the node name as the node id, this should be set to False if add_nodes is True
     """
+
+    if add_nodes and use_name_as_id:
+        raise ValueError("use_name_as_id should be False if add_nodes is True")
 
     G = epanet(inp_file)
 
-    node_ids = {}  # EPANET node index to PYPES node id
+    node_ids = {} # EPANET node index to PYPES node id
     nodes = {}
     connections = {}
 
@@ -46,12 +51,16 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
         "Reservoir": 0,
         "Pipe": 0,
         "Pump": 0,
+        "Prv": 0,
     }
 
     for n in G.getNodeIndex():
         # Node type is one of: Junction, Reservoir, Tank
         if G.getNodeType(n).upper() == "JUNCTION":
-            id_str = "Joint" + str(obj_counts["Joint"] + 1)
+            if use_name_as_id:
+                id_str = G.getNodeNameID(n)
+            else:
+                id_str = "Joint" + str(obj_counts["Joint"]+1)
             node_obj = {
                 "id": id_str,
                 "type": "Joint",
@@ -63,7 +72,10 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
             obj_counts["Joint"] += 1
 
         elif G.getNodeType(n).upper() == "RESERVOIR":
-            id_str = "Reservoir" + str(obj_counts["Reservoir"] + 1)
+            if use_name_as_id:
+                id_str = G.getNodeNameID(n)
+            else:
+                id_str = "Reservoir" + str(obj_counts["Reservoir"]+1)
             node_obj = {
                 "id": id_str,
                 "type": "Reservoir",
@@ -76,9 +88,12 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
             obj_counts["Reservoir"] += 1
 
         elif G.getNodeType(n).upper() == "TANK":
-            id_str = "Tank" + str(obj_counts["Tank"] + 1)
+            if use_name_as_id:
+                id_str = G.getNodeNameID(n)
+            else:
+                id_str = "Tank" + str(obj_counts["Tank"]+1)
             node_obj = {
-                "id": id_str,
+                "id":id_str,
                 "type": "Tank",
                 "contents": content_placeholder,
                 "levation (meters)": G.getNodeElevations(n),
@@ -94,7 +109,7 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
 
     for connection in G.getLinkIndex():
         if add_nodes:
-            # Link type is one of: Pipe, Pump, Valve
+            # Link type is one of: Pipe, Pump, Valve, Prv
             if G.getLinkType(connection).upper() == "PIPE":
                 connection_obj = {
                     "id": "Pipe" + str(obj_counts["Pipe"]),
@@ -108,10 +123,13 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
                 obj_counts["Pipe"] += 1
 
             elif G.getLinkType(connection).upper() == "PUMP":
+                # TODO: seperate pump into 2 pipes and a pump
                 pump_obj1 = {
-                    "id": "Pump" + str(obj_counts["Pump"]),
+                    "id": "Pump" + str(obj_counts["Pump"]), 
                     "type": "Pump",
                     "contents": content_placeholder,
+                    "source": node_ids[G.getLinkNodesIndex(connection)[0]],
+                    "destination": "Pipe" + str(connection),
                     "tags": {},
                 }
                 nodes["Pump" + str(obj_counts["Pump"])] = pump_obj1
@@ -122,68 +140,66 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
                     "type": "Pipe",
                     "contents": content_placeholder,
                     "source": node_ids[G.getLinkNodesIndex(connection)[0]],
-                    "destination": "Pump" + str(obj_counts["Pump"]),
-                    "tags": {},
-                }
-                connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
-                obj_counts["Pipe"] += 1
-
-                connection_obj = {
-                    "id": "Pipe" + str(obj_counts["Pipe"]),
-                    "type": "Pipe",
-                    "contents": content_placeholder,
-                    "source": "Pump" + str(obj_counts["Pump"]),
                     "destination": node_ids[G.getLinkNodesIndex(connection)[1]],
                     "tags": {},
                 }
                 connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
                 obj_counts["Pipe"] += 1
 
+                pump_obj2 = {
+                    "id": "Pump" + str(obj_counts["Pump"]),
+                    "type": "Pump",
+                    "contents": content_placeholder,
+                    "source": "Pipe" + str(connection),
+                    "destination": node_ids[G.getLinkNodesIndex(connection)[1]],
+                    "tags": {},
+                }
+                nodes["Pump" + str(obj_counts["Pump"])] = pump_obj2
+                obj_counts["Pump"] += 1
+
             elif G.getLinkType(connection).upper() == "VALVE":
-                raise NotImplementedError("Valves not yet supported in PyPES")
-                # TODO: create Valve object
-                # then seperate valve into multiple pipes and a valve
+                # TODO: seperate valve into multiple pipes and a Joint
                 # Assign the first node to source, and the other nodes to destination
-                # sources = []
-                # destinations = []
-                # for i, linknode in enumerate(G.getLinkNodesIndex(connection)):
-                #     if i == 0:
-                #         sources.append(node_ids[linknode])
-                #     else:
-                #         destinations.append(node_ids[linknode])
+                sources = []
+                destinations = []
+                for i, linknode in enumerate(G.getLinkNodesIndex(connection)):
+                    if i == 0:
+                        sources.append(node_ids[linknode])
+                    else:
+                        destinations.append(node_ids[linknode])
 
-                # joint_obj = {
-                #     "id": "Joint" + str(obj_counts["Joint"]),
-                #     "type": "Joint",
-                #     "contents": content_placeholder,
-                #     "tags": {},
-                # }
-                # nodes["Joint" + str(obj_counts["Joint"])] = joint_obj
-                # obj_counts["Joint"] += 1
+                joint_obj = {
+                    "id": "Joint" + str(obj_counts["Joint"]),
+                    "type": "Joint",
+                    "contents": content_placeholder,
+                    "tags": {},
+                }
+                nodes["Joint" + str(obj_counts["Joint"])] = joint_obj
+                obj_counts["Joint"] += 1
 
-                # for source in sources:
-                #     connection_obj = {
-                #         "id": "Pipe" + str(obj_counts["Pipe"]),
-                #         "type": "Pipe",
-                #         "contents": content_placeholder,
-                #         "source": source,
-                #         "destination": "Joint" + str(obj_counts["Joint"] - 1),
-                #         "tags": {},
-                #     }
-                #     connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
-                #     obj_counts["Pipe"] += 1
-
-                # for destination in destinations:
-                #     connection_obj = {
-                #         "id": "Pipe" + str(obj_counts["Pipe"]),
-                #         "type": "Pipe",
-                #         "contents": content_placeholder,
-                #         "source": "Joint" + str(obj_counts["Joint"] - 1),
-                #         "destination": destination,
-                #         "tags": {},
-                #     }
-                #     connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
-                #     obj_counts["Pipe"] += 1
+                for source in sources:
+                    connection_obj = {
+                        "id": "Pipe" + str(obj_counts["Pipe"]),
+                        "type": "Pipe",
+                        "contents": content_placeholder,
+                        "source": source,
+                        "destination": "Joint" + str(obj_counts["Joint"] - 1),
+                        "tags": {},
+                    }
+                    connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
+                    obj_counts["Pipe"] += 1
+                
+                for destination in destinations:
+                    connection_obj = {
+                        "id": "Pipe" + str(obj_counts["Pipe"]),
+                        "type": "Pipe",
+                        "contents": content_placeholder,
+                        "source": "Joint" + str(obj_counts["Joint"] - 1),
+                        "destination": destination,
+                        "tags": {},
+                    }
+                    connections["Pipe" + str(obj_counts["Pipe"])] = connection_obj
+                    obj_counts["Pipe"] += 1
 
             else:
                 raise ValueError(
@@ -192,7 +208,10 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
         else:
             type_str = G.getLinkType(connection).upper()
             type_str = type_str[0].upper() + type_str[1:].lower()
-            id_str = type_str + str(connection)
+            if use_name_as_id:
+                id_str = G.getLinkNameID(connection)
+            else:
+                id_str = type_str + str(connection)
             connection_obj = {
                 "id": id_str,
                 "type": "Pipe",
@@ -204,6 +223,8 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
             connections[id_str] = connection_obj
             obj_counts[type_str] += 1
 
+    print(obj_counts)
+    
     data = {
         "nodes": list(nodes.keys()),
         "connections": list(connections.keys()),
@@ -221,10 +242,12 @@ def epyt2pypes(inp_file, out_file, add_nodes=False):
     with open(out_file, "w") as f:
         json.dump(data, f, indent=2, cls=NpEncoder)
 
-    return data
-
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    epyt2pypes(args[0], args[1])
-    print("EPANET to PYPES conversion from {} to {} complete".format(args[0], args[1]))
+    epyt2pypes(
+        "json/others/L-Town.inp", 
+        "json/L-Town.json", 
+        add_nodes=False, 
+        use_name_as_id=True
+    )
+    print("EPANET to PYPES conversion complete.")
