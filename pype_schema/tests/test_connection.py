41b3ad7b96ea8ec7a3d2b524cf3576d716ceff39
Fletcher Chapin <fletchapin@gmail.com>
2025-08-05 15:54:00 -0700

    Update test_connection.py to fix flake8 errors

--- pype_schema/tests/test_connection.py
+++ pype_schema/tests/test_connection.py
@@ -332,7 +332,7 @@ def test_source_dest_nodes(json_path, connection_name, source_id, dest_id, recur
 
 @pytest.mark.skipif(skip_all_tests, reason="Exclude all tests")
 @pytest.mark.parametrize(
-    "json_path, conn_id", 
+    "json_path, conn_id",
     [("data/connection.json", "GasToCogen")]
 )
 def test_get_fallback_error(json_path, conn_id):
