8ed4ae33ec84be8ad4e711640e4171b6b3c78c5c
Fletcher Chapin <fletchapin@gmail.com>
2026-06-15 14:15:29 -0700

    Bump version: 0.7.2 → 0.8.0

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     extras_require=extra_requirements,
     test_suite="tests",
     url="https://github.com/we3lab/pype-schema",
-    version="0.7.2",
+    version="0.8.0",
     zip_safe=False,
 )
